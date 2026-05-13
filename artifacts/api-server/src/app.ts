53e2dbe9d0ba25431c0b5928b385c04fcb4a9c89
afmi5241-7 <afmi5241@gmail.com>
2026-05-13 16:41:24 +0000

    rename routes folder via terminal to fix cache

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -2,7 +2,7 @@ import express, { type Express } from "express";
 import cors from "cors";
 import session from "express-session";
 import pinoHttp from "pino-http";
-import routes from "./routes/index.js";
+import routes from "./api_routes/index.js";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
