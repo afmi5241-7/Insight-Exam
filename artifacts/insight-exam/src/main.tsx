8c4675c58cef486903e39c2a725863545ab4ce75
afmi5241-7 <afmi5241@gmail.com>
2026-05-14 10:20:04 +0000

    1234

--- artifacts/insight-exam/src/main.tsx
+++ artifacts/insight-exam/src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 // استيراد دالة تحديد الرابط من المكتبة التي وجدناها قبل قليل
 import { setBaseUrl } from "../../../lib/api-client-react/src";
 
-// تحديد رابط سيرفر Render ليكون هو الوجهة الأساسية لكل البيانات
+// تأكد من الرابط السحابي الصحيح للسيرفر (الباك إند)
 setBaseUrl("https://insight-exam.onrender.com");
 
 createRoot(document.getElementById("root")!).render(<App />);
